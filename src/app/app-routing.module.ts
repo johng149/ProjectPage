f1ea2307c36d5b94ae6ce437e0738e5cadef91d6
johnguo149 <johnguo149@gmail.com>
2021-11-11 18:53:03 -0500

    Added basic GAN page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { SimilarityComponent } from './projects/similarity/similarity.component';
+import { GanComponent } from './projects/gan/gan.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "similarity", component: SimilarityComponent},
+  {path: "gan", component: GanComponent},
   {path: "about", component: AboutComponent},
   {path: "**", component: HomeComponent, pathMatch: "full"}
 ];
